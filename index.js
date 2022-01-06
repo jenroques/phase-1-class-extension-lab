576649e87f786420684589cbbba9995c4269cb1c
Jennifer Roques <jenniferroques@gmail.com>
2022-01-05 22:11:43 -0600

    commit tree 84cf0e6a33ec36a2838f997c0b5f157cd649eaf1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+
+}
+
+class Triangle extends Polygon {
+    //constructor(triSides) {
+
+    // }
+}
+
+
+class Square extends Polygon {
+    /*constructor(squSides) {
+        if (squSides > 0) {
+            this.squSides = squSides;
+        } else {
+            return false;
+        }
+    }
+
+    get squSides() {
+        this.squSides = Math.sqrt(this.squSides)
+    }
+
+*/
+}
+
+
+
+
+
